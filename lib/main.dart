7634638e747fa2f99ddb2d29912ab24a0659c6f3
Mohamed Ahmed Ramzy <modtrz@hotmail.com>
2026-06-26 14:37:34 +0300

    Update app version to 1.0.29

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ import 'wifi_config_page.dart';
 import 'auth_service.dart';
 import 'login_screen.dart';
 
-const String appVersion = '1.0.13';
+const String appVersion = '1.0.29';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
