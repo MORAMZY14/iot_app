9b49d64384bce6a18cf960188261f9f7d0984912
Mohamed Ahmed Ramzy <modtrz@hotmail.com>
2026-06-27 22:33:54 +0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "F:\\Smart-Home\\iot-app\\iot_app-master" PROJECT_DIR)
+
+set(FLUTTER_VERSION "2.0.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 2 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\flutter"
+  "PROJECT_DIR=F:\\Smart-Home\\iot-app\\iot_app-master"
+  "FLUTTER_ROOT=E:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=F:\\Smart-Home\\iot-app\\iot_app-master\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=F:\\Smart-Home\\iot-app\\iot_app-master"
+  "FLUTTER_TARGET=F:\\Smart-Home\\iot-app\\iot_app-master\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9WRVJTSU9OPTMuNDQuMg==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049YzlhNmM0ODQyMw==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NzdlMmU5NDc3Mg==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMi4y"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=F:\\Smart-Home\\iot-app\\iot_app-master\\.dart_tool\\package_config.json"
+)
