5edf0b44c81c989ab804dba0f447c68e484ed273
Mohamed Ahmed Ramzy <modtrz@hotmail.com>
2026-07-17 13:19:36 -0700

    Update app version to V2.0.5

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ import 'splash_screen.dart';  // 🔥 NEW: Import your splash screen
 import 'login_screen.dart';
 import 'app_constants.dart';
 
-const String appVersion = AppConfig.appVersion;
+const String appVersion = 'V2.0.5';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
